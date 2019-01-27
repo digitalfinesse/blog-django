a6427d587d42a848dd78dc4a827df02e8ad6fb94
digitalfinesse <dizverst@gmail.com>
2019-01-28 01:56:16 +0300

    Add model for comments

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from news.models import News, Category, Tag
+from news.models import News, Category, Tag, Comments
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(News)
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text_min', 'text')
+
+admin.site.register(News, NewsAdmin)
+
 admin.site.register(Category)
 admin.site.register(Tag)
+
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'new', 'created', 'moderation')
+
+
+
+
+
+admin.site.register(Comments, CommentAdmin)
+
+
+
+
+
